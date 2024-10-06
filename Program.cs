4db62d94835fa9bb6509d5d593f47a2e39b3da40
MortezaQarani <mortazaqarani@gmail.com>
2024-10-06 20:27:33 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        public static void Add(int input1, int input2)
+        {
+            Console.WriteLine(input1 + input2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter num1:");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter num2:");
+            int num2=int.Parse(Console.ReadLine());
+            Add(input1: num1, input2: num2);
+
+        }
+    }
+}

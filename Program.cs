2cfaaa44868add7f0cf65e1d009fb21c265d8272
MortezaQarani <mortazaqarani@gmail.com>
2024-10-27 18:37:14 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student feature = new Student();
+            feature.Name = "Ali".ToLower();
+            feature.fatherName = "Ahmad".ToLower();
+            feature.age = 20;
+            feature.average = 19.0;
+            feature.isMale = true;
+
+            feature.Name = "Zahra".ToLower();
+            feature.fatherName = "hasan".ToLower();
+            feature.age = 19;
+            feature.average = 20.0;
+            feature.isMale = false;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Student
+    {
+        public string Name{ get; set; }
+        public string fatherName { get; set; }
+        public int age { get; set; }
+
+        public double average { get; set; }
+        public bool isMale { get; set; }
+    }
+}
